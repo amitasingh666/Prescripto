e2981db766e88676bd344a0a3d3afd81f399af6a
amitasingh <amitasingh3173@gmail.com>
2025-07-29 03:14:53 +0530

    addDoctor in admin connected to backend

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
     }
 
   return (
-    <div className='flex justify-between items-center h-12 sm:px-10 w-320 border-b bg-white'>
+    <div className='flex justify-between items-center h-12 sm:px-10 w-316 border-b bg-white'>
         <div className='flex item-center gap-2 text-xs' style={{marginLeft:25}}>
-            <img className='w-36 sm:w-40 cursor-pointer' src={assets.logo} alt="" />
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
             <p style={{marginTop:10}} className='border h-5 w-15 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
         </div>
         <button onClick={logout} style={{marginRight:25}} className='bg-[#6366F1] text-white text-sm h-8 w-28 rounded-full cursor-pointer'>Logout</button>
