7c7e0fbc42bf5abb26bc5ff47887d96463a1326d
amitasingh <amitasingh3173@gmail.com>
2025-07-28 22:30:28 +0530

    login api tested and admin setup

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
+import {useNavigate} from 'react-router-dom'
+
+const Navbar = () => {
+
+    const {aToken, setAToken} = useContext(AdminContext)
+
+    const navigate = useNavigate()
+
+    const logout = () => {
+        navigate('/')
+        aToken && setAToken('')
+        aToken && localStorage.removeItem('aToken')
+    }
+
+  return (
+    <div className='flex justify-between items-center h-12 sm:px-10 w-320 border-b bg-white'>
+        <div className='flex item-center gap-2 text-xs' style={{marginLeft:25}}>
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.logo} alt="" />
+            <p style={{marginTop:10}} className='border h-5 w-15 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
+        </div>
+        <button onClick={logout} style={{marginRight:25}} className='bg-[#6366F1] text-white text-sm h-8 w-28 rounded-full cursor-pointer'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useContext, useState } from 'react'
+
+import {assets} from '../assets/assets'
+import {AdminContext} from '../context/AdminContext'
+
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const Login = () => {
+
+    const [state, setState] = useState('Admin')
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {setAToken, backendUrl} = useContext(AdminContext)
+
+    const onSubmitHandler = async(event) => {
+
+        event.preventDefault()
+        
+        try {
+            
+            if (state === 'Admin') {
+                
+                const {data} = await axios.post(backendUrl + '/api/admin/login', {email, password})
+                if (data.success) {
+                    localStorage.setItem('aToken', data.token)
+                    setAToken(data.token);
+                    
+                } else {
+                    toast.error(data.message)
+                }
+
+            } else {
+
+            }
+
+        } catch (error) {
+            
+        }
+    }
+
+
+  return (
+    <form onSubmit={onSubmitHandler} className='h-80 w-30 flex item-center' style={{marginLeft:450, marginTop:100}}>
+        <div className='flex flex-col gap-3 m-auto p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
+            <p style={{marginLeft:100, marginTop:40}} className='text-2xl font-semibold m-auto'><span className='text-[#5F6FFF]'> {state} </span> Login</p>
+            <div style={{marginLeft:30, marginTop:5}} className='w-80'>
+                <p>Email</p>
+                <input onChange={(e) => setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-80 p-2 mt-1 h-8' type="email" required />
+
+            </div>
+
+            <div style={{marginLeft:30, marginTop:5}} className='w-80'>
+                <p>Password</p>
+                <input onChange={(e) => setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-80 h-8 p-2 mt-1' type="password" required />
+
+            </div>
+
+            <button style={{marginLeft:30, marginTop:5}} className='bg-[#5F6FFF] text-white w-80 h-8 rounded-md text-base cursor-pointer'>Login</button>
+
+            {
+                state === 'Admin'
+                ? <p style={{marginLeft:30, marginTop:5}}>Doctor Login? <span  className='text-[#5F6FFF] cursor-pointer underline' onClick={() => setState('Doctor')}>Click Here</span> </p>
+                : <p style={{marginLeft:30, marginTop:5}}> Admin Login? <span style={{marginLeft:30, marginTop:5}} className='text-[#5F6FFF] cursor-pointer underline' onClick={() => setState('Admin')}>Click Here</span> </p>
+            }
+
+        </div>
+    </form>
+  )
+}
+
+export default Login
